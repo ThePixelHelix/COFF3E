3d6c7a7ffa69c88e6ec5ed10bef352cf79dfef8b
ThePixelHelix <67651217+ThePixelHelix@users.noreply.github.com>
2021-01-03 22:29:24 +0200

    Update music.py

--- commands/music.py
+++ commands/music.py
@@ -465,7 +465,7 @@ class Music(commands.Cog):
         This command automatically searches from various sites if no URL is provided.
         A list of these sites can be found here: https://rg3.github.io/youtube-dl/supportedsites.html
         """
-
+    @commands.command(name='play')
         if not ctx.voice_state.voice:
             await ctx.invoke(self._join)
 
